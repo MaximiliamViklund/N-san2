e6bcd35635d5dd0c77662748eb6f9b5206348dad
Maximiliam Viklund TE22E <143697940+MaximiliamViklund@users.noreply.github.com>
2024-11-07 15:40:31 +0100

    Färdig

--- Näsan2/Program.cs
+++ Näsan2/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿List<string> cities=new();
+bool exit=false;
+string resp;
+while(!exit){
+    Console.WriteLine("Write something to be entered into \"cities\" or write \"exit\" to exit");
+    resp=Console.ReadLine().ToLower();
+    if(resp=="exit"){
+        exit=true;
+    }
+    else{
+        cities.Add(resp);
+        Console.Clear();
+    }
+}
+for (int i = 0; i < cities.Count; i++){
+    Console.WriteLine(cities[i]);
+}
+Console.ReadLine();
